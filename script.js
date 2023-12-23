cbcc87db292586626a519f751c9d89a3ec456ae7
Soni kumari <Sonikumari78707@gmail.com>
2023-12-23 18:57:25 +0530

    Update 23 D

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-console.log("hello word");
-let a=alert("sms");
+const getColor = () =>{
+	const randomNumber = Math.floor(Math.random() * 16777215);
+	// console.log(randomNumber);
+	const randomCode= "#" + randomNumber.toString(16);
+	// console.log(randomNumber ,randomCode);
+	document.body.style.backgroundColor=randomCode;
+
+	document.getElementById("color-code").innerText = randomCode;
+
+	// automatic click board pe random color store
+	navigator.clipboard.writeText(randomCode)
+	
+}
+
+//event call
+document.getElementById("btn")
+.addEventListener("click",getColor
+
+)
+
+// init call reload-color-call
+// getColor();
+
+
+
+
+
+
+
+
